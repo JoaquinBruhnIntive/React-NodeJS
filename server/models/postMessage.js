61419252db995aec6edb5a55a7262db798ec0220
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2022-02-08 15:58:15 -0300

    first commit

---
+++ server/models/postMessage.js
@@ -0,0 +1,21 @@
+import Mongoose from "mongoose";
+
+const postScheema = Mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = Mongoose.model("PostMessage", postScheema);
+
+export default PostMessage;
